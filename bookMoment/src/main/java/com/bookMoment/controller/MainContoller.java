a100e259c5fcd9cb64079f1747b1e6d54f77270d
hoon <mipoung@gmail.com>
2024-09-16 12:31:06 +0900

    main 컨트롤러 주석 추가

--- bookMoment/src/main/java/com/bookMoment/controller/MainContoller.java
+++ bookMoment/src/main/java/com/bookMoment/controller/MainContoller.java
@@ -19,7 +19,7 @@ public class MainContoller {
 
 	@GetMapping("/")
 	public String mainView(Model model) {
-		 List<MainDTO> resultList = mainService.test();
+		 List<MainDTO> resultList = mainService.test(); // 아직 DB 생성 전이라 DB 미생성시 오류 날 수 있음, 오류 시 주석처리 후 실행
 		System.out.println(resultList);
 		
 		model.addAttribute("result", resultList);
